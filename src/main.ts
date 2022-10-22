f59b0de5eed0cea2fcc1ea21359e204f7f9ac5a3
Arnaldo tomo <arnaldotomo@gmail.com>
2022-10-22 20:05:13 +0200

    Babdkiyuisis

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,6 @@ import { environment } from './environments/environment';
 if (environment.production) {
   enableProdMode();
 }
-
+//jgioyfgyufiyofvfyidtiilbiuo
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
